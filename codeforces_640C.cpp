511a42170a8729a968324ff7251f8a27957d5a79
rsrahul1000 <rsrahul1000@gmail.com>
2020-05-10 00:05:09 +0530

    codeforces problem 640 C of div 4

---
+++ codeforces_640C.cpp
@@ -0,0 +1,20 @@
+// Created by Rahul Sharma
+#include <bits/stdc++.h>
+
+#define loop(i,n) for(int i=0;i<(n);++i)
+#define repA(i,a,n) for(int i=a;i<=(n);++i)
+#define repD(i,a,n) for(int i=a;i>=(n);--i)
+
+using namespace std;
+using ll = long long;
+
+int main(){
+    ll t, n, k;
+    cin >> t;
+    while(t--){
+        cin >> n >> k;
+        if(k%(n-1) == 0) cout << n*((k/(n-1))-1) + (n-1) << endl;
+        else cout << n*(k/(n-1)) + (k%(n-1)) << endl;
+    }
+    return 0;
+}
