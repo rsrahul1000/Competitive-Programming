96a796510b651280eae07d0e05b9f030b44a2e71
rsrahul1000 <rsrahul1000@gmail.com>
2020-05-12 19:44:18 +0530

    timecomplexity solved by using unordered_set data structure

--- codeforces_1283C.cpp
+++ codeforces_1283C.cpp
@@ -14,28 +14,33 @@ int main()
 	cout.tie(0);
 	int n;
 	cin >> n;
-	vector<int> v(n), f(n + 1), z,q;
+	vector<int> v(n), f(n + 1);
+	unordered_set<int> q, z;	//this takes O(1) to insert an element
 	rep(i, n) {
 		cin >> v[i];
 		f[v[i]]++;
-		if (v[i] == 0) z.push_back(i+1);
+		if (v[i] == 0) z.insert(i+1);
 	}
-	rep(i,n+1) if (f[i]==0) q.push_back(i); 
+	rep(i,n+1) if (f[i]==0) q.insert(i); 
  
 	int t = 0;
-	for (int i=0; i<z.size(); i++){
-		for(int j=0; j<q.size(); j++){
-			if(z[i]==q[j] && q.size()==1){
-				v[z[i]-1] = v[t];
-				v[t] = q[j];
+	//for (int i=0; i<z.size(); i++){
+	for (int x:z ) {
+		//for(int j=0; j<q.size(); j++){
+		for (int y:q ) {
+			if(x==y && q.size()==1){
+				v[x-1] = v[t];
+				v[t] = y;
 			}
-			else if(z[i]==q[j]){
+			else if(x==y){
 				continue;
 			}
 			else{
-				v[z[i]-1] = q[j];
-				t = z[i]-1;
-				q.erase(q.begin() + j);
+				v[x-1] = y;
+				t = x-1;
+				auto it = q.find(y);
+				q.erase(it);
+				//q.erase(q.begin() + j);
 				break;
 			}
 		}
