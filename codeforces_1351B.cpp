2c0b73edff3966dcd20e08309838f27234efb5b4
Rahul Sharma <rsrahul1000@gmail.com>
2020-05-20 00:39:20 +0530

    working fine

--- codeforces_1351B.cpp
+++ codeforces_1351B.cpp
@@ -17,7 +17,6 @@ int main()
     cin>>t;
     while(t--){
         cin>>a1 >> b1>> a2>> b2;
-
     	if ( (a1==a2) && (b1+b2==a1) || (a1 == b2)&&(b1+a2==a1) || (b1==a2)&&(a1+b2==a2) ||
     		(b1 == b2)&&(a1+a2==b2)) cout << "YES";
     		else cout << "NO";
