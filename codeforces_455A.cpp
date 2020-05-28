476f5af96a094e497b926af5753f53b8a975c26b
Rahul Sharma <rsrahul1000@gmail.com>
2020-05-28 20:05:47 +0530

    second DP solution, understandable

--- codeforces_455A.cpp
+++ codeforces_455A.cpp
@@ -62,35 +62,62 @@
 #include <bits/stdc++.h>
 
 #define rep(i,n) for(int i=0;i<(n);++i)
-#define repA(i,a,n) for(int i=a;i<=(n);++i)
-#define repD(i,a,n) for(int i=a;i>=(n);--i)
 
 using namespace std;
-using ll = long long int;
+using ll = long long;
 
+// int main()
+// {
+//     ios::sync_with_stdio(0);
+//     cin.tie(0);
+//     cout.tie(0);
+//     ll n, x, a, b, t;
+//     cin>>n;
+//     vector<ll> v(12); //1e5+1);
+
+//     rep(i, n) {
+//     	cin >> x;
+//     	v[x] += x;
+//     }
+//     for (int i: v) cout << i << " ";
+//     cout << endl;
+//     a=b=0;
+//     for(ll i: v){
+//     	cout << i << " " << a << " " << b << " " << b+i <<  endl;
+//     	t = a;
+//     	a = max(a, i+b);
+//     	b = t;
+//     	cout << i << " " << a << " " << b << " " << b+i<< endl << endl;
+//     }
+
+//     cout << a << endl;
+//     return 0;
+// }
+
+
+#define repA(i,a,b) for (ll i = (a), _b = (b); i <= _b; i++)
+#define N (int)1e5 + 5
+#define M (int)2e6 + 5
+#define OO 0x3f3f3f3f
+ll dp[N], fr[N];
+int n;
+ll id;
 int main()
 {
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-    cout.tie(0);
-    ll n, x, a, b, t;
-    cin>>n;
-    vector<ll> v(100001);
-
-    rep(i, n) {
-    	cin >> x;
-    	v[x] += x;
+    cin >> n;
+    rep(i, n)
+    {
+        ll x;
+        cin >> x;
+        fr[x]++;
     }
-    // for (int i: v) cout << i << " ";
-    // cout << endl;
-    a=b=0;
-    for(ll i: v){
-    	t = a;
-    	a = max(a, i+b);
-    	b = t;
-    	//cout << a << " " << i << " " << b << endl;
+    dp[1] = fr[1];
+    repA(i, 2, N - 1)
+    {
+        dp[i] = max(dp[i - 1], dp[i - 2] + fr[i] * i);
     }
-
-    cout << a << endl;
+    cout << dp[N - 1] << endl;
+    //cout << (double(clock() - tt)/CLOCKS_PER_SEC * 1.00) << endl;
+    //system("sleep 50");
     return 0;
 }
